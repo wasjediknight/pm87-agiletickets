3db0b449e6d703374bb948336a6bcc335bd04370
agil6790 <agil6790@caelumBSB03-04>
2017-06-20 16:28:49 -0300

    ¨Corrigidobugvagas¨

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -38,4 +38,13 @@ public class SessaoTest {
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
 	
+	
+	@Test
+	public void DeveVender100ingressoSeHa100vagas() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(100);
+
+		Assert.assertTrue(sessao.podeReservar(100));
+	}
+	
 }
