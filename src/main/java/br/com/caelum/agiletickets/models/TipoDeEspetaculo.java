46b71c752747fc6b2562e6bc111e9f60716deb08
agil6790 <agil6790@caelumBSB03-04>
2017-06-21 15:20:41 -0300

    ¨Refactory¨

--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -1,7 +1,14 @@
 package br.com.caelum.agiletickets.models;
 
+import br.com.caelum.agiletickets.domain.precos.CalculadoraDePreco;
+
 public enum TipoDeEspetaculo {
 	
 	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
+
+	public CalculadoraDePreco getCalculadorDePreco() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
